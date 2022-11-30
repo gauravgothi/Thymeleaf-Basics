60be845d514fa83cf33b75857d186c301ec68cee
gauravgothi <gauravgothi.patidar@gmail.com>
2022-11-30 17:14:53 +0530

    loop in thymeleaf with status attribute

--- src/main/java/com/gaurav/springboot/thymeleaf/Controller/MyController.java
+++ src/main/java/com/gaurav/springboot/thymeleaf/Controller/MyController.java
@@ -2,10 +2,12 @@ package com.gaurav.springboot.thymeleaf.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MyController {
@@ -18,5 +20,12 @@ public class MyController {
         model.addAttribute("currentDate",new Date().toString());
 
         return "about";
+    }
+    @RequestMapping(value = "/example-loop",method=RequestMethod.GET)
+    public String iterateHandler(Model model)  {
+        //create list
+        List<String> stringList = List.of("Ankit","Jaya","Gaurav","Sujit");
+        model.addAttribute("names",stringList);
+        return "example-loop";
     }
 }
